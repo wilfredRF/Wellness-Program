4fbdb6ca923c986887966f1cfd39e7aadf028800
David Mbagwu <150841790+DavidMbagwu@users.noreply.github.com>
2024-02-10 08:49:12 -0600

    Add files via upload

---
+++ Hackerthon/homepage.py
@@ -0,0 +1,95 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import random
+from journal import *
+
+# This is the home screen of the wellness app
+
+root = tk.Tk()
+
+root.geometry("400x500")
+root.title("Wellness Program")
+
+
+def home_page(username):
+    for widgets in root.winfo_children():
+        widgets.destroy()
+    print("Hey " + username.get())
+    greetings = username.get()
+    bg_home = tk.PhotoImage(file="Wellness/meditate8 (1).png")  # Suitable size: W x H: 1540 x 1040 px
+
+    journal = tk.PhotoImage(file="Wellness/study.png")
+
+    quote = tk.PhotoImage(file="Wellness/quote.png")
+
+    studying = tk.PhotoImage(file="Wellness/journal.png")
+
+    background_home = tk.Label(root, image=bg_home)
+
+    background_home.place(relheight=1, relwidth=1)
+
+    # Text: 'Hello, what's your name?'
+
+    text = tk.Label(root, text=" Hello "+greetings+" ", font=("Pristina", 40), background="#2C5F2D",
+                    foreground="#97BC62", borderwidth=1, relief="raised")
+    text.pack(padx=5, pady=15)
+
+    text1 = tk.Label(root, text=" What would you like to do today 😄 ", font=("Pristina", 40), background="#2C5F2D",
+                    foreground="#97BC62", borderwidth=1, relief="raised")
+    text1.pack(padx=5, pady=15)
+
+    memoir_button = tk.Button(root, text=" Personal Journal  😸 ", command= lambda: textEditor() )
+    memoir_button.place(x=240, y=680)
+
+    quote_button = tk.Button(root, text=" Quote of the Day 😇 ")
+    quote_button.place(x=740, y=680)
+
+    study_button = tk.Button(root, text=" Study 🤩 ")
+    study_button.place(x=1280, y=680)
+
+    memoir_img = tk.Label(root, image=journal,width=400, height=350)
+
+    quote_img = tk.Label(root, image=quote, width=400, height=350)
+
+    study_img = tk.Label(root, image=studying, width=400, height=350)
+
+    memoir_img.place(x=110, y=320)
+    quote_img.place(x=610, y=320)
+    study_img.place(x=1110, y=320)
+
+    root.mainloop()
+
+
+def switch(page):
+    page()
+
+
+# Background image
+
+bg = tk.PhotoImage(file="Wellness/meditate9 (1).png")  # Suitable size: W x H: 1540 x 1040 px
+
+background = tk.Label(root, image=bg)
+
+background.place(relheight=1, relwidth=1)
+
+# Text: 'Hello, what's your name?'
+
+text = tk.Label(root, text=" Hello, what's your name? ", font=("Pristina", 40), background="#2C5F2D",
+                foreground="#97BC62", borderwidth=1, relief="raised")
+text.pack(padx=5, pady=15)
+
+# Frame for the name input and next button
+name_box = tk.Frame(root, bg="#2C5F2D", borderwidth=1, relief="raised")
+
+# Collect name
+user_name = tk.StringVar()
+name = tk.Entry(name_box, font=("Pristina"), textvariable=user_name)
+name.grid(padx=5, pady=5, row=0, column=0)
+
+# Next button
+
+next_button = tk.Button(name_box, text="Next", command=lambda: switch(home_page(user_name)))
+next_button.grid(padx=5, pady=5, row=0, column=1)
+
+name_box.pack()
+root.mainloop()
---
+++ Hackerthon/journal.py
@@ -0,0 +1,123 @@
+import tkinter as tk
+import tkinter.font as tkFont
+def textEditor():
+    root = tk.Tk()
+
+    root.title('Personal Journal Entry')
+    root.geometry("600x600")
+
+    # Create the menu: THis will contain 'File' 'Format' 'search'
+    menu = tk.Menu(root)
+    root.configure(menu=menu)
+    root.configure(background="#5c9aef")
+    file_menu = tk.Menu(menu, tearoff=0, foreground="pink", background="#5c9aef")
+    label = tk.Label(root, text="Personal Journal 🤠", font=("Georgia",32), background="#003f39", foreground="White")
+    label.pack(padx=5, pady=10)
+    menu.add_cascade(label="File", menu=file_menu)
+
+    file_menu.add_command(label="New", command=lambda: textEditor())
+    file_menu.add_command(label="Open", command=lambda: textEditor())
+    file_menu.add_separator()
+
+    format_menu = tk.Menu(menu, tearoff=0, foreground="pink", background="#5c9aef")
+    bold_sub_menu = tk.Menu(format_menu, tearoff=0, foreground="#5c9aef")
+    format_menu.add_cascade(label="Bold", menu=bold_sub_menu)
+    bold_sub_menu.add_radiobutton(label="Bold On", value="on", command=lambda: print("hello"))
+    bold_sub_menu.add_radiobutton(label="Bold Off", value="off", command=lambda: print("Bye"))
+
+    italic_sub_menu = tk.Menu(format_menu, tearoff=0, foreground="#5c9aef")
+    format_menu.add_cascade(label="Italics", menu=italic_sub_menu)
+    italic_sub_menu.add_radiobutton(label="Italics On", value="on1", command=lambda: print("hello"))
+    italic_sub_menu.add_radiobutton(label="Italics Off", value="off1", command=lambda: print("Bye"))
+
+    underline_sub_menu = tk.Menu(format_menu, tearoff=0, foreground="#5c9aef")
+    format_menu.add_cascade(label="Underline", menu=underline_sub_menu)
+    underline_sub_menu.add_radiobutton(label="Underline On", value="on2", command=lambda: print("hello"))
+    underline_sub_menu.add_radiobutton(label="Underline Off", value="off2", command=lambda: print("Bye"))
+
+    def font_menu():
+        fonts = list(tkFont.families())
+        fonts.sort()
+        print(fonts)
+        listbox = tk.Listbox(root, selectmode=tk.SINGLE)
+        for font in fonts:
+            listbox.insert('end', font)
+
+        listbox.place(x=100, y=20)
+
+        def font_chooser(event):
+            our_font.configure(
+                family=listbox.get(listbox.curselection())
+            )
+            listbox.destroy()
+
+        listbox.bind('<ButtonRelease>', font_chooser)
+
+    def font_size_menu():
+        font_size = [2, 8, 12, 14, 16, 18, 24, 32, 48, 64, 96, 128]
+        listbox = tk.Listbox(root, selectmode=tk.SINGLE)  # 'select mode' controls how many objects can be selected [
+        # SINGLE, EXTENDED, e.t.c]
+
+        i = 0
+        while i < len(font_size):
+            listbox.insert('end', str(font_size[i]))  # Always make sure to add the 'end' index in the
+            # front
+            i += 1
+        listbox.place(x=100, y=20)
+
+        def font_chooser(event):
+            our_font.configure(
+                size=int(listbox.get(listbox.curselection()))
+            )
+            listbox.destroy()
+
+        listbox.bind('<ButtonRelease>', font_chooser)  # This carries out the function called after the given command
+
+    def font_color_menu():
+        font_color = ['Red', 'Green', 'Blue', 'Brown', 'Yellow', 'Black', 'White', 'Orange', 'Gray', 'Pink', 'Purple',
+                      'Olive', 'Gold', 'Cyan']
+        listbox = tk.Listbox(root, selectmode=tk.SINGLE)
+        i = 0
+        while i < len(font_color):
+            listbox.insert('end', str(font_color[i]))
+            i += 1
+        listbox.place(x=100, y=20)
+
+        def color_chooser(event):
+            text.configure(
+                foreground=(listbox.get(listbox.curselection()))
+            )
+            listbox.destroy()
+
+        listbox.bind('<ButtonRelease>', color_chooser)
+
+    # font_sub_menu = tk.Menu(format_menu)
+    format_menu.add_command(label="Font", command=font_menu)
+
+    format_menu.add_command(label="Font-Size", command=font_size_menu)
+
+    format_menu.add_command(label="Font-Color", command=font_color_menu)
+
+    format_menu.add_separator()
+
+    search_menu = tk.Menu(menu, tearoff=0)
+    menu.add_cascade(label="Format", menu=format_menu)
+    menu.add_cascade(label="Search", menu=search_menu)
+
+    # Frame for the text box
+    frame = tk.Frame(root, width=510, height=900)
+    frame.pack(pady=10)
+
+    # Set the font attributes to 'our_font' so it can be configured/ manipulated in the functions above
+    our_font = tkFont.Font(family="Helvetica", size=12, )
+
+    #  Freeze frame in place
+    frame.propagate(False)
+    frame.columnconfigure(0, weight=10)
+
+    text = tk.Text(frame, font=our_font)
+    text.pack()
+
+    root.mainloop()
+
+
